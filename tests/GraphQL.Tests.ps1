888c3efa4494c9c8debc375cd06d5610873b1b6d
Marius Storhaug <marstor@hotmail.com>
2024-03-20 23:03:12 +0100

    init

---
+++ tests/GraphQL.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe 'GraphQL' {
+    Context 'Module' {
+        It 'The module should be available' {
+            Get-Module -Name 'GraphQL' -ListAvailable | Should -Not -BeNullOrEmpty
+            Write-Verbose (Get-Module -Name 'GraphQL' -ListAvailable | Out-String) -Verbose
+        }
+        It 'The module should be importable' {
+            { Import-Module -Name 'GraphQL' } | Should -Not -Throw
+        }
+    }
+}
