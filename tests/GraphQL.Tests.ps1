53aa48954d9601f67bdd1baba244f8f4ebbf2937
Marius Storhaug <marstor@hotmail.com>
2024-08-22 00:19:57 +0200

    🚀 [Feature]: Restructure for Process-PSModule v3 (#6)
    ## Description
    
    - Removing support for Windows PowerShell - directional change for
    PSModule.
    - Remove nuget from Dependabot settings
    - Using Process-PSModule v3
    - Restructure for v3
    
    ## Type of change
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [ ] 📖 [Docs]
    - [ ] 🪲 [Fix]
    - [ ] 🩹 [Patch]
    - [ ] ⚠️ [Security fix]
    - [x] 🚀 [Feature]
    - [ ] 🌟 [Breaking change]
    
    ## Checklist
    
    <!-- Use the check-boxes [x] on the options that are relevant. -->
    
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas

--- tests/GraphQL.Tests.ps1
+++ tests/GraphQL.Tests.ps1
@@ -1,11 +1,10 @@
 ﻿Describe 'GraphQL' {
-    Context 'Module' {
-        It 'The module should be available' {
-            Get-Module -Name 'GraphQL' -ListAvailable | Should -Not -BeNullOrEmpty
-            Write-Verbose (Get-Module -Name 'GraphQL' -ListAvailable | Out-String) -Verbose
-        }
-        It 'The module should be importable' {
-            { Import-Module -Name 'GraphQL' } | Should -Not -Throw
+    Context 'Get-PSModule' {
+        It 'should greet the world' {
+            $Name = 'World'
+            $Expected = "Hello, $Name!"
+            $Actual = Get-PSModule -name $Name
+            $Actual | Should -Be $Expected
         }
     }
 }
