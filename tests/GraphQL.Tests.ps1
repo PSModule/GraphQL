76146aa8ce77b92d19c0d7e1a1edfb3949563327
Marius Storhaug <marstor@hotmail.com>
2026-07-07 17:45:37 +0200

    Require Pester 6.x in test files

--- tests/GraphQL.Tests.ps1
+++ tests/GraphQL.Tests.ps1
@@ -1,4 +1,6 @@
-﻿Describe 'GraphQL' {
+﻿#Requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '6.0.0'; MaximumVersion = '6.*'; GUID = 'a699dea5-2c73-4616-a270-1f7abb777e71' }
+
+Describe 'GraphQL' {
     Context 'Get-PSModule' {
         It 'should greet the world' {
             $Name = 'World'
